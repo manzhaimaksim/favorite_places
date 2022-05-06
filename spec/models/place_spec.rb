06ca170ce8e4ec47ce240777e2df1437442e5140
matveibos <matveibos@mail.ru>
2022-05-06 13:04:51 +0300

    adding tests

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user)}
+  it { should have_many(:comments)}
+  it { should have_many(:place_likes)}
+  it { should have_many(:users)}
+  it { should have_many(:photos)}
 end
