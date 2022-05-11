6e881765a64cf580fbd6ccd42c905037a044d7d3
manzhaimaksim <maxbbb88@gmail.com>
2022-05-11 19:52:08 +0300

    editing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'places#index'
+  resources :places, only: [:index]
   resources :users, only: [:show]
   devise_for :users, controllers: { registrations: 'users/registrations' }, path: '', path_names: { sign_in: 'login', registration: 'registration' }
 end
