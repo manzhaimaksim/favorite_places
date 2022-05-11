e6d3aa5f6cbb53f06355bb534fddd33ad4f28cf9
manzhaimaksim <maxbbb88@gmail.com>
2022-05-11 23:48:21 +0300

    generate files for bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import {} from 'jquery-ujs'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
