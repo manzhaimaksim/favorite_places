0aadf15bee29f64916bbdd9d6ee742056c217b2f
manzhaimaksim <maxbbb88@gmail.com>
2022-05-11 19:52:00 +0300

    editing response tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,32 +1,12 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  describe "GET /new" do
+  describe "GET /registration/sign_up" do
     it "returns http success" do
-      get "/users/new"
+      get "/registration/sign_up"
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/users/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
