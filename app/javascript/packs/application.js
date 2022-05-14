8ddf2e8c7f3fd0209aa5ed4b5a6a42d96ae1c7df
manzhaimaksim <maxbbb88@gmail.com>
2022-05-14 18:39:13 +0300

    editing bootstrap settings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,17 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import {} from 'jquery-ujs'
-import 'stylesheets/application'
+import 'bootstrap/dist/js/bootstrap';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+
+require.context('../images', true);
+console.log('Hello World from Webpacker');
