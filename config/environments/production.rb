08d3bdcb724e736602e6284ffd813d7ccddad60f
manzhaimaksim <maxbbb88@gmail.com>
2022-05-17 20:14:27 +0300

    editing production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.delivery_method = :smtp
-  host = 'herokuapp.com' #replace with your own url
+  host = 'stark-caverns-50270.herokuapp.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
