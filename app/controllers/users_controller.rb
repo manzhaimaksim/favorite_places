2889824c108efcbabf52146e41a860234126e93d
manzhaimaksim <maxbbb88@gmail.com>
2022-05-22 21:28:57 +0300

    adding generate json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.any(:xml, :json) { render request.format.to_sym => @user.places }
+    end
   end
 end
