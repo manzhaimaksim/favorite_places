2c89201f522f51516699258e5d8652609c781f2b
manzhaimaksim <maxbbb88@gmail.com>
2022-05-28 14:00:37 +0300

    adding resourses photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root to: 'places#index'
-  resources :places
+  root to: "places#index"
+  resources :places do
+    resources :photos, only: %i[create destroy]
+  end
   resources :users, only: [:show]
-  devise_for :users, controllers: { registrations: 'users/registrations' }, path: '', path_names: { sign_in: 'login', registration: 'registration' }
+  devise_for :users, controllers: {registrations: "users/registrations"}, path: "",
+                     path_names: {sign_in: "login", registration: "registration"}
 end
