6af67bfb956336238ace510cff4198465339516a
manzhaimaksim <maxbbb88@gmail.com>
2022-05-28 14:00:18 +0300

    adding uploader for Avatar to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :notifications, dependent: :destroy
   has_many :places
   has_many :comments, dependent: :destroy
-  # has_many :places, through: :comments
   has_many :liked_places, through: :likes, as: :place
   has_many :photos, as: :entity
   has_many :subscribers, foreign_key: :subscriber_id
@@ -16,4 +15,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :date_of_birth, presence: true
+
+  mount_uploader :avatar, AvatarUploader
 end
