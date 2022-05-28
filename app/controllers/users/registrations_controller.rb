81b0741777e7fe491aab951a28a172cae08a9ae3
manzhaimaksim <maxbbb88@gmail.com>
2022-05-28 13:59:17 +0300

    adding permit params for update User

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -20,9 +20,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -42,13 +42,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    params.require(:user).permit(:username, :email, :name, :sex, :date_of_birth, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :name, :sex, :date_of_birth, :avatar, :password, :password_confirmation)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :name, :sex, :date_of_birth, :avatar, :password, :password_confirmation])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
