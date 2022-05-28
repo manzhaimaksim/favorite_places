f2dbbd8c15f77c70024600b87080f8ac22eefc98
manzhaimaksim <maxbbb88@gmail.com>
2022-05-28 11:30:32 +0300

    editin Place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Place < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user, required: true
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :photos, as: :entity, dependent: :destroy
