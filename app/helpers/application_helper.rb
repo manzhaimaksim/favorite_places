145ef9741c1f02d5133fdbbe167849ef6b098f9f
manzhaimaksim <maxbbb88@gmail.com>
2022-05-28 13:59:26 +0300

    adding helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar?
+      user.avatar.url
+    else
+      asset_path('user.jpg')
+    end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.jpg')
+    end
+  end
 end
