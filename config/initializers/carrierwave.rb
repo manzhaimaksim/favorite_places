cde72a97fb9601c1a18d06864fbf9fe12b48f311
manzhaimaksim <maxbbb88@gmail.com>
2022-05-29 20:28:23 +0300

    change config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      use_iam_profile:       true,
+      use_iam_profile:       false,
       host:                  's3.example.com',
       endpoint:              'https://s3.example.com:8080'
     }
