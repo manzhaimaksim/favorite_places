90491e15c0b07b4086fad5b5a2e4d41299dfac02
manzhaimaksim <maxbbb88@gmail.com>
2022-05-29 16:22:59 +0300

    editing tests

--- spec/requests/photos_spec.rb
+++ spec/requests/photos_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Photos", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/photos/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/photos/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
