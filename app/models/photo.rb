3db000ee4138acb5d3c9b308aeba1180001ba805
manzhaimaksim <maxbbb88@gmail.com>
2022-05-29 15:20:13 +0300

    adding validation in Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,10 @@
 class Photo < ApplicationRecord
-  belongs_to :entity, polymorphic: true
+  belongs_to :user
+  belongs_to :place
+
+  validates :photo, presence: true
+  validates :title, presence: true
+  validates :alt, presence: true
+
+  mount_uploader :photo, PhotoUploader
 end
