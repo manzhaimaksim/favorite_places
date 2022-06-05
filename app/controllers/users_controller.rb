a457dbde5328b67236f7c3bcf184bdf226e914a3
manzhaimaksim <maxbbb88@gmail.com>
2022-06-05 16:28:46 +0300

    add method favorite for Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,19 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
+  before_action :set_user, except: %i[index]
 
   def show
-    @user = User.find(params[:id])
-
     gon.users_places = @user.places
+  end
+
+  def favorite
+    @places_likes = Place.joins("INNER JOIN likes ON likes.place_id = places.id")
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
