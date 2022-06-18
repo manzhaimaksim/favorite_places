88b9c73ce543fdd719470441cca8eb592e11e5f1
manzhaimaksim <maxbbb88@gmail.com>
2022-06-18 19:30:01 +0300

    editing config for mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,11 +124,11 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['GMAIL_PROD_USERNAME'],
-    :password             => ENV['GMAIL_PROD_PASSWORD'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD_16'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
+    password: ENV['GMAIL_PASSWORD_16'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
