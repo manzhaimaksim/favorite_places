47520db1ee7c2f23503e8a78f6280dc0765ae786
manzhaimaksim <maxbbb88@gmail.com>
2022-06-26 21:40:14 +0300

    adding locale scope for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: "places#index"
-  resources :places do
-    resources :photos, only: %i[create destroy]
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: "places#index"
+    resources :places do
+      resources :photos, only: %i[create destroy]
+    end
+    resources :users, only: [:show]
+    devise_for :users, controllers: {registrations: "users/registrations"}, path: "",
+                      path_names: {sign_in: "login", registration: "registration"}
+    put "/places/:id/like", to: "places#like", as: "like"
+    put "/places/:id/dislike", to: "places#dislike", as: "dislike"
+    get "/users/:id/favorite", to: "users#favorite", as: "favorite"
   end
-  resources :users, only: [:show]
-  devise_for :users, controllers: {registrations: "users/registrations"}, path: "",
-                     path_names: {sign_in: "login", registration: "registration"}
-  put "/places/:id/like", to: "places#like", as: "like"
-  put "/places/:id/dislike", to: "places#dislike", as: "dislike"
-  get "/users/:id/favorite", to: "users#favorite", as: "favorite"
 end
